dd9c855ab65ad283d01d731075620935aed88061
Sergio Campos <morrodogg8@gmail.com>
2019-07-16 16:09:56 -0700

    Initial Commit

---
+++ p3.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p3.2
+//
+//  Created by Sergio campos on 7/16/19.
+//  Copyright © 2019 Sergio campos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
