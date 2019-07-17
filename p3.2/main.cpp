4ecf0f0361ba0849877d0683330f8012e5beaeb5
Sergio Campos <morrodogg8@gmail.com>
2019-07-17 02:36:45 -0700

    finished

--- p3.2/main.cpp
+++ p3.2/main.cpp
@@ -1,15 +1,57 @@
-//
-//  main.cpp
-//  p3.2
-//
-//  Created by Sergio campos on 7/16/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ •• P3.2
+ Write a program that translates a number between 0 and 4 into the closest letter grade. For example, the number 2.8 (which might have been the average of several grades) would be converted to B–. Break ties in favor of the better grade; for example 2.85 should be a B.
+ */
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "Enter grade number: ";
+    float number;
+    cin >> number;
+    int grade = number;
+    float tie_breaker = (number - grade) * 100;
+    string letter, suffix;
+    
+    
+    //cout << number << " " << grade << " " << tie_breaker << "\n";
+    
+    if (number >= 3.7)
+    {
+        letter = "A";
+        if (number < 4)
+        {
+            suffix = "-";
+        }
+    }
+    else if (number >= 2.7)
+    {
+        letter = "B";
+    }
+    else if (number >= 2)
+    {
+        letter = "C";
+    }
+    else if (number >= 0.7)
+    {
+        letter = "D";
+    }
+    else
+    {
+        letter = "F";
+    }
+    if (tie_breaker >= 85 && tie_breaker <= 99.9)
+    {
+        suffix = "-";
+    }
+    else if (tie_breaker > 0.1 && tie_breaker< 70)
+    {
+        suffix = "+";
+    }
+    cout << letter << suffix << endl;
 }
